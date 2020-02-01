418150a23a465d7c8fdfcc84ae87700aa678518c
Michal <misko_elias@yahoo.com>
2020-02-01 19:44:10 +0100

    redesing

--- Dependencies/json-3.5.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/michal/Work/gitHub/Casper/Dependencies/json-3.5.0
-# Build directory: /home/michal/Work/gitHub/Casper/Dependencies/json-3.5.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
